90a95f71901beb4ae974d02021672b7d457079ed
AFFROJ SHAIK <affroj1574786@gmail.com>
2026-02-24 23:39:41 +0530

    Remove unused message state

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -22,7 +22,7 @@ const [name, setName] = useState("");
 const [email, setEmail] = useState("");
 const [password, setPassword] = useState("");
 
-  const [message, setMessage] = useState("");
+ 
 
  
 
